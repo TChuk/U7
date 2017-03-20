8662bced2486f157c0d3e67cbb52eb4143af9242
Ryan Tabachuk <neji@cyrena.ca>
2017-03-20 18:27:29 +0000

    Part 2: added a new controller with three actions and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get '/', to: 'finder#index', as: 'index'
+
+  get 'customers/alphabetized', to: 'finder#alphabetized', as: 'ordered'
+
+  get 'customers/missing_email', to: 'finder#missing_email', as: 'email'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root to: 'finder#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
